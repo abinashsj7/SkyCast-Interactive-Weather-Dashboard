f0df7826642d205b3dd9aac961da95ecf8a563a5
abinashsj7 <sit23cs075@sairamtap.edu.in>
2025-09-24 19:05:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // Replace with your actual OpenWeatherMap API key (between quotes).
-const OPENWEATHER_API_KEY = "32f91f1d4678e3d091a4f52ac9454bca";
+const OPENWEATHER_API_KEY="32f91f1d4678e3d091a4f52ac9454bca";
